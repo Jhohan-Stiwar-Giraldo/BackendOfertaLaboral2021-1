5669353b0b1ffc3a3ac043be727d273ff2d876ff
Jhohan Stiwar Giraldo <jhohan.1701821033@ucaldas.edu.co>
2021-03-22 12:18:03 -0500

    creacion de modelo y data sources

---
+++ src/models/tipo-usuario.model.ts
@@ -0,0 +1,29 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class TipoUsuario extends Entity {
+
+  @property({
+    type: 'number',
+    id: true,
+    generated: true,
+  })
+  id?: number;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  nombre: string;
+
+
+  constructor(data?: Partial<TipoUsuario>) {
+    super(data);
+  }
+}
+
+export interface TipoUsuarioRelations {
+  // describe navigational properties here
+}
+
+export type TipoUsuarioWithRelations = TipoUsuario & TipoUsuarioRelations;
